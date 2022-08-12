f512583b2d92edae0870bad017f36ba92095c0cf
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-08-12 14:14:29 +0000

    updated the ui components

--- my-project/src/components/UI/ErrorNotification.js
+++ my-project/src/components/UI/ErrorNotification.js
@@ -1,5 +1,5 @@
 const ErrorNotification = () => {
-  return <div>Oops! Something went wrong. Pls try again.</div>;
+  return <span className="text-red-600 font-medium font-read text-lg">Oops! Something went wrong. Pls try again.</span>;
 };
 
 export default ErrorNotification;
