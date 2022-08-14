ba4fa998864d766981782bf8594ec4e08bb980f8
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-08-14 22:34:38 +0000

    wrote tests ✔

---
+++ my-project/src/components/AssetItem.test.js
@@ -0,0 +1,41 @@
+import { render, screen } from "@testing-library/react";
+import { MockedProvider } from "@apollo/client/testing";
+import { ASA_LIST } from "../queries/asaListQuery";
+import AssetItem from "./AssetItem";
+
+const mocks = [
+  {
+    request: {
+      query: ASA_LIST,
+    },
+    result: {
+      data: {
+        asalist: {
+          results: [
+            {
+              assetId: "1",
+              name: "ASA 1",
+              available: true,
+              logo: "https://www.asa.org/images/asa-logo.png",
+            },
+          ],
+        },
+      },
+    },
+  },
+];
+
+test("renders without error", async () => {
+  render(
+    <MockedProvider mocks={mocks} addTypename={false}>
+      <AssetItem
+        imageSrc="https://www.asa.org/images/asa-logo.png"
+        name="ASA 1"
+        availability={true}
+      />
+    </MockedProvider>
+  );
+  expect(
+    await screen.findByText('ASA 1')
+  ).toBeInTheDocument();
+});
