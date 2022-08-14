7d4d12555f78315c6bf708c44820d43d05244f38
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-08-14 15:48:57 +0000

    typescript template

---
+++ my-project-ts/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      read: ["Readex Pro", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
